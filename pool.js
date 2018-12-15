6e661b3fe3f5dedaeb1be8a58c48948da1b68bfb
Catherine1004 <44466618+Catherine1004@users.noreply.github.com>
2018-12-15 16:57:56 +0800

    js

---
+++ pool.js
@@ -0,0 +1,12 @@
+//连接mysql服务器
+const mysql=require('mysql');
+var pool=mysql.createPool({
+	host:'127.0.0.1',
+	port:3306,
+	user:'root',
+	password:'',
+	database:'ps',
+	connectLimit:20
+})
+//导出连接
+module.exports=pool;
